5d43b9171ce962b2b7852a6ad12801860f098e2f
Snow <njwittig183@gmail.com>
2026-02-03 16:22:37 +0100

    Enhance Postfix method with null and count checks
    Refactor Postfix method to handle null checks and improve safety.

--- Source/1.6/AnimationMod/Patches/Patch_FloatMenuMakerMap_GetOptions.cs
+++ Source/1.6/AnimationMod/Patches/Patch_FloatMenuMakerMap_GetOptions.cs
@@ -15,9 +15,15 @@ public class Patch_FloatMenuMakerMap_GetOptions
     [UsedImplicitly]
     private static void Postfix(Vector3 clickPos, List<Pawn> selectedPawns, List<FloatMenuOption> __result)
     {
-        if (selectedPawns.Count != 1)
-            return;
-        
-        __result.AddRange(DraftedFloatMenuOptionsUI.GenerateMenuOptions(clickPos, selectedPawns[0]));
+        if (__result == null)
+            __result = new List<FloatMenuOption>();
+        if (selectedPawns == null || selectedPawns.Count != 1)
+            return false;
+        Pawn pawn = selectedPawns[0];
+
+        if (pawn == null || !pawn.Spawned || pawn.Map == null)
+            return false;
+        __result.AddRange(DraftedFloatMenuOptionsUI.GenerateMenuOptions(clickPos, pawn));
+        return true;
     }
 }
